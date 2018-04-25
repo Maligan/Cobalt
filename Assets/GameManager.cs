d1e700a7cec1e5178fadf1589fc91bb3a4974133
Alexandr Frolov <maligan@rambler.ru>
2018-04-25 20:53:08 +0300

    Add basic project structs: GameManager/Prefabs

---
+++ Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	// Prefabs
+	public GameObject Wall;
+	public GameObject Unit;
+	public GameObject Bomb;
+	
+	// Active objects
+	public GameObject P1;
+	
+	private void Start()
+	{
+		CreateLevel();
+		CreateUnit();
+	}
+
+	public void CreateLevel()
+	{
+		var width = 5;
+		var height = 3;
+
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				var wall = Instantiate(Wall);
+				wall.name = string.Format("{0} ({1}; {2})", Wall.name, x, y);
+				wall.transform.Translate(x*2, y*2, 0);
+			}
+		}
+	}
+
+	public void CreateUnit()
+	{
+		P1 = Instantiate(Unit);
+		P1.transform.Translate(-1, -1, 0);
+	}
+}
