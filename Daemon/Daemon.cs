72e0f96921e1946513782599f7c6fa6eb46dc7c4
Alexandr Frolov <maligan@rambler.ru>
2021-07-14 01:45:57 +0300

    Cleanup Daemon main method

--- Daemon/Daemon.cs
+++ Daemon/Daemon.cs
@@ -6,40 +6,30 @@ namespace Cobalt
 {
     public class Daemon
     {
-        private LanServer _server;
-
         public static void Main(string[] args)
         {
-            new Daemon().Start();
-        }
+            var server = new LanServer();
 
-        private void Start()
-        {
-            Log.Info(this, "Start...");
-            _server = new LanServer();
-            _server.Start(new ShardOptions());
-            Tick();
-        }
-
-        private async void Tick()
-        {
-            var start = DateTime.Now;
-            var step = 1000f/_server.Options.TPS;
-            var frame = start;
-
-            while (_server.IsRunning)
+            while (true)
             {
-                var now = DateTime.Now;
-                
-                var sinceFrame = (now - frame).TotalMilliseconds;
-                if (sinceFrame > step)
+                server.Start(new ShardOptions());
+
+                var start = DateTime.Now;
+                var step = 1000f/server.Options.TPS;
+                var frame = start;
+
+                while (server.IsRunning)
                 {
-                    frame = now;
-                    _server.Update((float)(now - start).TotalSeconds);
+                    var now = DateTime.Now;
+                    
+                    var sinceFrame = (now - frame).TotalMilliseconds;
+                    if (sinceFrame > step)
+                    {
+                        frame = now;
+                        server.Update((float)(now - start).TotalSeconds);
+                    }
                 }
             }
-
-            _server.Stop();
         }
     }
 }
