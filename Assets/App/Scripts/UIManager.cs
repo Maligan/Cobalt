d42903a4233fb6bfa12af7ec27bb70ce8aa87ca6
Alexandr Frolov <maligan@rambler.ru>
2021-07-14 13:14:17 +0300

    Hide UIManger internal API

--- Assets/App/Scripts/UIManager.cs
+++ Assets/App/Scripts/UIManager.cs
@@ -5,143 +5,166 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Playables;
 
-namespace Cobalt.UI
+public class UIManager : MonoBehaviour
 {
-    public class UIManager : MonoBehaviour
+    [SerializeField]
+    private List<Transform> _roots = null;
+
+    private Dictionary<Type, UIBehaviour> _elements;
+    private Dictionary<UIBehaviour, Coroutine> _elementsCoroutine;
+    private List<Relation> _relations;
+    private bool _relationsIsDirty;
+
+    public UIManager()
     {
-        [SerializeField]
-        private List<Transform> _roots = null;
+        _elements = new Dictionary<Type, UIBehaviour>();
+        _elementsCoroutine = new Dictionary<UIBehaviour, Coroutine>();
+        _relations = new List<Relation>();
+    }
 
-        private Dictionary<Type, UIPanel> _panels;
-        private Dictionary<UIPanel, Coroutine> _coroutines;
-        private List<Relation> _relations;
-        private bool _relationsIsDirty;
+    private void Start()
+    {
+        foreach (var root in _roots)
+            RegisterAll(root);
+    }
 
-        public UIManager()
+    public void RegisterAll(Transform root)
+    {
+        var components = root.GetComponentsInChildren<UIBehaviour>(true);
+
+        foreach (var element in components)
+            Register(element);
+    }
+
+    public void Register(UIBehaviour element)
+    {
+        Debug.Assert(
+            element.transform.parent == null ||
+            element.transform.parent.GetComponentInParent<UIPanel>() == null,
+            $"UIPanel '{element.name}' mustn't be nested"
+        );
+
+        _elements.Add(element.GetType(), element);
+        ((IUIBehaviour)element).UI = this;
+        element.gameObject.SetActive(false);
+    }
+
+    public T Get<T>() where T : UIBehaviour
+    {
+        var elementType = typeof(T);
+
+        if (_elements.ContainsKey(elementType) == false)
+            throw new Exception($"{nameof(UIPanel)} with type '{elementType.Name}' not found");
+
+        return (T)_elements[elementType];
+    }
+
+    private void Require(UIBehaviour element, object token, int state)
+    {
+        var relation = _relations.FirstOrDefault(x => x.element == element && x.token == token);
+        if (relation == null && state != 0)
         {
-            _panels = new Dictionary<Type, UIPanel>();
-            _coroutines = new Dictionary<UIPanel, Coroutine>();
-            _relations = new List<Relation>();
-        }
-
-        private void Start()
-        {
-            foreach (var root in _roots)
-                RegisterAll(root);
-        }
-
-        public void RegisterAll(Transform root)
-        {
-            var components = root.GetComponentsInChildren<UIPanel>(true);
-
-            foreach (var panel in components)
-               Register(panel);
-        }
-
-        public void Register(UIPanel panel)
-        {
-            Debug.Assert(
-                panel.transform.parent?.GetComponentInParent<UIPanel>() == null,
-                $"UIPanel '{panel.name}' mustn't be nested"
-            );
-
-            _panels.Add(panel.GetType(), panel);
-            panel.UIManager = this;
-            panel.gameObject.SetActive(false);
-        }
-
-        public T Get<T>() where T : UIPanel
-        {
-            var panelType = typeof(T);
-
-            if (_panels.ContainsKey(panelType) == false)
-                throw new Exception($"{nameof(UIPanel)} with type '{panelType.Name}' not found");
-
-            return (T)_panels[panelType];
-        }
-
-        internal void Require(UIPanel panel, object token, int state)
-        {
-            var relation = _relations.FirstOrDefault(x => x.panel == panel && x.token == token);
-            if (relation == null && state != 0)
+            // Create new relation
+            _relationsIsDirty = true;
+            _relations.Add(new Relation()
             {
-                // Create new relation
-                _relationsIsDirty = true;
-                _relations.Add(new Relation()
-                {
-                    panel = panel,
-                    token = token,
-                    state = state
-                });
-            }
-            else if (relation != null && relation.state != state)
-            {
-                // Update exist relation
-                _relationsIsDirty = true;
-                relation.state = state;
-            }
+                element = element,
+                token = token,
+                state = state
+            });
         }
-
-        private void Update()
+        else if (relation != null && relation.state != state)
         {
-            if (_relationsIsDirty)
-            {
-                _relationsIsDirty = false;
-
-                // Calculate UIPanels strongest relation
-                var dominants = new Dictionary<UIPanel, Relation>();
-                foreach (var relation in _relations)
-                {
-                    var needUpdate = !(dominants.ContainsKey(relation.panel))
-                                  || !(dominants[relation.panel] > relation);  
-
-                    if (needUpdate) dominants[relation.panel] = relation;
-                }
-
-                // Apply relations
-                foreach (var relation in dominants.Values)
-                {
-                    // Cancel previous coroutine (if it's not completed)
-                    var hasCoroutine = _coroutines.TryGetValue(relation.panel, out Coroutine coroutine);
-                    if (hasCoroutine && coroutine != null)
-                        StopCoroutine(coroutine);
-                    
-                    _coroutines[relation.panel] = StartCoroutine(relation.Apply());
-                }
-            }
+            // Update exist relation
+            _relationsIsDirty = true;
+            relation.state = state;
         }
+    }
 
-        private class Relation
+    private void Update()
+    {
+        if (_relationsIsDirty)
         {
-            public object token;
-            public UIPanel panel;
-            public int state;
+            _relationsIsDirty = false;
 
-            public IEnumerator Apply()
+            // Calculate UIBehaviours strongest relation
+            var dominants = new Dictionary<UIBehaviour, Relation>();
+            foreach (var relation in _relations)
             {
-                if (state > 0 && !panel.IsShow)
-                    yield return panel.ShowRoutine();
-                else if (state <= 0 && panel.IsShow)
-                    yield return panel.HideRoutine();
+                var needUpdate = !(dominants.ContainsKey(relation.element))
+                                || !(dominants[relation.element] > relation);  
+
+                if (needUpdate) dominants[relation.element] = relation;
             }
 
-            public static bool operator <(Relation r1, Relation r2) { return !(r1 > r2); }
-            public static bool operator >(Relation r1, Relation r2)
+            // Apply relations
+            foreach (var relation in dominants.Values)
             {
-                if (r1.token != r2.token) return false;
-                if (r1.panel != r2.panel) return false;
-                return Mathf.Abs(r1.state) > Mathf.Abs(r2.state);
+                // Cancel previous coroutine (if it's not completed)
+                var hasCoroutine = _elementsCoroutine.TryGetValue(relation.element, out Coroutine coroutine);
+                if (hasCoroutine && coroutine != null)
+                    StopCoroutine(coroutine);
+                
+                _elementsCoroutine[relation.element] = StartCoroutine(relation.Apply());
             }
         }
     }
 
-    [RequireComponent(typeof(RectTransform))]
-    public class UIPanel : MonoBehaviour
+    // Tool class for toggle UIBehaviour state
+    private class Relation
     {
-        // Internal API
-        internal UIManager UIManager { get; set; }
+        public object token;
+        public UIBehaviour element;
+        public int state;
 
-        internal IEnumerator ShowRoutine()
+        public IEnumerator Apply()
+        {
+            if (state > 0 && !element.IsShow)
+                yield return ((IUIBehaviour)element).Show();
+            else if (state <= 0 && element.IsShow)
+                yield return ((IUIBehaviour)element).Hide();
+        }
+
+        public static bool operator <(Relation r1, Relation r2) { return !(r1 > r2); }
+        public static bool operator >(Relation r1, Relation r2)
+        {
+            if (r1.token != r2.token) return false;
+            if (r1.element != r2.element) return false;
+            return Mathf.Abs(r1.state) > Mathf.Abs(r2.state);
+        }
+    }
+
+    // Hidden API for UIManager <-> UIBehaviour interaction
+    private interface IUIBehaviour
+    {
+        UIManager UI { get; set; }
+        IEnumerator Show();
+        IEnumerator Hide();
+    }
+
+    // Base class for any UI element
+    public abstract class UIBehaviour : MonoBehaviour, IUIBehaviour
+    {
+        // Public API
+
+        public bool IsShow { get; private set; }
+        public bool IsTransit { get; private set; }
+
+        public T Get<T>() where T : UIBehaviour => ((IUIBehaviour)this).UI.Get<T>();
+        public void Require(object token, int state) { ((IUIBehaviour)this).UI.Require(this, token, state); }
+        public void Show() { Require(this, 1); }
+        public void Hide() { Require(this, 0); }
+
+        // Protected template methods
+
+        protected virtual IEnumerator OnShow() { yield break; }
+        protected virtual IEnumerator OnHide() { yield break; }
+
+        // IUIBehaviour
+        
+        UIManager IUIBehaviour.UI { get; set; }
+
+        IEnumerator IUIBehaviour.Show()
         {
             gameObject.SetActive(true);
             IsShow = true;
@@ -150,7 +173,7 @@ namespace Cobalt.UI
             IsTransit = false;
         }
 
-        internal IEnumerator HideRoutine()
+        IEnumerator IUIBehaviour.Hide()
         {
             IsShow = false;
             IsTransit = true;
@@ -158,60 +181,54 @@ namespace Cobalt.UI
             IsTransit = false;
             gameObject.SetActive(false);
         }
+    }
+}
 
-        // Show/Hide Routines
-        protected virtual IEnumerator OnShow() { yield break; }
-        protected virtual IEnumerator OnHide() { yield break; }
+[RequireComponent(typeof(RectTransform))]
+public class UIPanel : UIManager.UIBehaviour
+{
+    #if UNITY_EDITOR
+    public void OnValidate() { gameObject.name = GetType().Name; }
+    #endif
 
-        // Open/Close
-        public void Show() { Require(this, 1); }
-        public void Hide() { Require(this, 0); }
-        public void Require(object token, int state) { UIManager.Require(this, token, state); }
+    //
+    // Tools
+    //
+    protected IEnumerator PlayAndAwait(string state)
+    {
+        var animator = GetComponent<Animator>();
+        if (animator != null && animator.enabled)
+        {
+            animator.Play(state);
+            yield return null;
 
-        public bool IsShow { get; internal set; }
-        public bool IsTransit { get; internal set; }
+            var layer = animator.GetCurrentAnimatorStateInfo(0);
+            var layerTime = layer.length;
+            yield return new WaitForSeconds(layerTime);
+        }
+    }
 
-        #if UNITY_EDITOR
-        public void OnValidate() { gameObject.name = GetType().Name; }
-		#endif
-
-        // Tools
-        protected IEnumerator PlayAndAwait(string state)
+    protected IEnumerator PlayAndAwait(AnimationClip clip)
+    {
+        if (clip != null)
         {
             var animator = GetComponent<Animator>();
-            if (animator != null && animator.enabled)
-            {
-                animator.Play(state);
-                yield return null;
+            if (animator == null)
+                animator = gameObject.AddComponent<Animator>();
 
-                var layer = animator.GetCurrentAnimatorStateInfo(0);
-                var layerTime = layer.length;
-                yield return new WaitForSeconds(layerTime);
+            if (animator.enabled)
+            {
+                AnimationPlayableUtilities.PlayClip(animator, clip, out PlayableGraph graph);
+                yield return new WaitForSeconds(clip.length);
+                graph.Destroy();
             }
         }
-
-        protected IEnumerator PlayAndAwait(AnimationClip clip)
+        else
         {
-            if (clip != null)
-            {
-                var animator = GetComponent<Animator>();
-                if (animator == null)
-                    animator = gameObject.AddComponent<Animator>();
-
-                if (animator.enabled)
-                {
-                    AnimationPlayableUtilities.PlayClip(animator, clip, out PlayableGraph graph);
-                    yield return new WaitForSeconds(clip.length);
-                    graph.Destroy();
-                }
-            }
-            else
-            {
-                Debug.LogWarning("AnimationClip can't be null");
-            }
+            Debug.LogWarning("AnimationClip can't be null");
         }
-
-		public IEnumerator WaitForShow() { while (!IsShow || IsTransit) yield return null; }
-		public IEnumerator WaitForHide() { while (IsShow || IsTransit) yield return null; }
     }
+
+    public IEnumerator WaitForShow() { while (!IsShow || IsTransit) yield return null; }
+    public IEnumerator WaitForHide() { while (IsShow || IsTransit) yield return null; }
 }
